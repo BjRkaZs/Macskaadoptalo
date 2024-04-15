41f04a87a96e772301de218b032f79264ef9306f
BjRkaZs <bajuszrekazsofi@ktch.hu>
2024-04-15 10:42:52 +0200

    feature signup working

---
+++ src/reg.js
@@ -0,0 +1,24 @@
+const togglePasswordVisibility = (inputElement, toggleElement) => {
+    if (inputElement.type === "password") {
+        inputElement.type = "text";
+        toggleElement.innerHTML = '<i class="bi bi-eye"></i>'; 
+    } else {
+        inputElement.type = "password";
+        toggleElement.innerHTML = '<i class="bi bi-eye-slash"></i>';
+    }
+}
+
+const passwordInput1 = document.getElementById("pass");
+const togglePassword1 = document.getElementById("togglePassword1");
+const passwordInput2 = document.getElementById("passHit");
+const togglePassword2 = document.getElementById("togglePassword2");
+
+togglePassword1.addEventListener("click", () => {
+    togglePasswordVisibility(passwordInput1, togglePassword1);
+});
+
+togglePassword2.addEventListener("click", () => {
+    togglePasswordVisibility(passwordInput2, togglePassword2);
+});
+
+
